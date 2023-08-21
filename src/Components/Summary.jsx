090bfd7717a6ae56d8dd5c9fc41d7902f8dbf781
Crystal <crwainstock@gmail.com>
2023-08-21 09:58:16 +0200

    rendering first 10 meteor names

--- src/Components/Summary.jsx
+++ src/Components/Summary.jsx
@@ -11,13 +11,13 @@ function Summary() {
   }, []);
 
   const fetchData = async () => {
-    // setLoading(true);
+    setLoading(true);
     try {
       let data = await d3Fetch.csv(
         "https://gist.githubusercontent.com/uKiJo/8655699e6f0a64c84d25ad652a9ca072/raw/8ed19eadc38db9a5606d3831c1c717d6b5358920/meteorite-landing.csv"
       );
 
-      //   setLoading(false);
+      setLoading(false);
       console.log({ data });
 
       setData(data);
@@ -27,7 +27,7 @@ function Summary() {
     }
   };
 
-  const meteors = data.map((meteor) => {
+  const meteors = data.slice(0, 10).map((meteor) => {
     // console.log(meteor);
     return <div key={meteor.id}>{meteor.name}</div>;
   });
@@ -35,7 +35,13 @@ function Summary() {
   return (
     <div>
       <h2>Summary Content</h2>
-      <div>{meteors}</div>
+      {loading ? (
+        <div className="spinner-border text-warning" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      ) : (
+        <div>{meteors}</div>
+      )}
     </div>
   );
 }
