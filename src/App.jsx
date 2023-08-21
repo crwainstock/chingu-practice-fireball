fa06e3f2d1c3ae6340ee119a4772c80101dfa00d
Crystal <crwainstock@gmail.com>
2023-08-21 08:50:50 +0200

    created Summary component with placeholder text

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import Summary from "./Components/Summary";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <h1>Fireball App goes here.</h1>
+      <Summary />
     </div>
   );
 }
---
+++ src/Components/Summary.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function Summary() {
+  return <h2>This is where the summary will display.</h2>;
+}
+
+export default Summary;
