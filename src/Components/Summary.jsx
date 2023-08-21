e1807f1f78bec7839b1d20f07fba417a3f86ce4e
Crystal <crwainstock@gmail.com>
2023-08-21 09:14:48 +0200

    added loader for data with bootstrap, not loading, but data is being fetched

--- src/Components/Summary.jsx
+++ src/Components/Summary.jsx
@@ -4,11 +4,10 @@ import * as d3Fetch from "d3-fetch";
 
 function Summary() {
   const [data, setData] = useState([]);
-  //   const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     fetchData();
-    console.log(data);
   }, []);
 
   const fetchData = async () => {
@@ -17,16 +16,25 @@ function Summary() {
       let data = await d3Fetch.csv(
         "https://gist.githubusercontent.com/uKiJo/8655699e6f0a64c84d25ad652a9ca072/raw/8ed19eadc38db9a5606d3831c1c717d6b5358920/meteorite-landing.csv"
       );
-      //   let data = await results.json();
       console.log(data);
-      //   setLoading(false);
+      setLoading(false);
       return data;
     } catch (err) {
       console.log(err);
     }
   };
 
-  return <h2>This is where the summary will display.</h2>;
+  return (
+    <div>
+      <h2>Summary Content</h2>
+      {loading ? (
+        <div className="spinner-border text-warning" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      ) : (
+        <h3>Loading finished.</h3>
+      )}
+    </div>
+  );
 }
-
 export default Summary;
