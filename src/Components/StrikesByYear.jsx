1add57445c693c259e69a41da1044dbbef21a730
Crystal <crwainstock@gmail.com>
2023-08-21 15:20:42 +0200

    create StrikesByYear file, thanks Alex!

---
+++ src/Components/StrikesByYear.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import * as d3Fetch from "d3-fetch";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+function StrikesByYear() {}
