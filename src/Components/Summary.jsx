46b6b49f630c6b4ddbd39b33c52b28f32262e109
Crystal <crwainstock@gmail.com>
2023-08-21 10:44:48 +0200

    created sample doughnut chart with charts.js

--- src/Components/Summary.jsx
+++ src/Components/Summary.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import * as d3Fetch from "d3-fetch";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 function Summary() {
   const [data, setData] = useState([]);
@@ -32,6 +36,38 @@ function Summary() {
     return <div key={meteor.id}>{meteor.name}</div>;
   });
 
+  const sample = {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  //   const meteorType = data.map((meteor) => {
+  //     // console.log(meteor.recclass);
+  //     return meteor.recclass;
+  //   });
+
   return (
     <div>
       <h2>Summary Content</h2>
@@ -40,7 +76,14 @@ function Summary() {
           <span className="visually-hidden">Loading...</span>
         </div>
       ) : (
-        <div>{meteors}</div>
+        <div>
+          {meteors}
+          <Doughnut
+            //   options={...}
+            data={sample}
+            // {...props}
+          />
+        </div>
       )}
     </div>
   );
