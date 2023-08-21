f0b67e6889f4f1967680e1a3c0073b9a3883dcfc
Crystal <crwainstock@gmail.com>
2023-08-21 21:55:16 +0200

    update strikes by year graph styling

--- src/Components/StrikesByYear.jsx
+++ src/Components/StrikesByYear.jsx
@@ -91,36 +91,40 @@ function StrikesByYear() {
       {
         label: "Number of Strikes by Year",
         data: strikesCount,
-        backgroundColor: "rgb(1, 244, 255)",
+        backgroundColor: "rgb(12, 22, 79)",
       },
     ],
   };
 
   const chartOptions = {
+    layout: {
+      padding: 20,
+    },
     scales: {
       x: {
         ticks: {
-          color: "white",
+          color: "rgb(38,182,183)",
         },
       },
       y: {
         ticks: {
-          color: "white",
+          color: "rgb(38,182,183)",
         },
         title: {
           display: true,
           text: "Number of Strikes",
-          color: "white",
+          color: "rgb(12, 22, 79)",
         },
         beginAtZero: true,
       },
     },
+
     plugins: {
       legend: {
         labels: {
-          color: "wheat",
+          color: "rgb(12, 22, 79)",
           font: {
-            size: 16, // Set the font size of the legend label text to 18
+            size: 18, // Set the font size of the legend label text to 18
           },
         },
       },
@@ -133,7 +137,7 @@ function StrikesByYear() {
         width: 700,
         color: "white",
         marginTop: 50,
-        background: "rgb(12, 22, 79)",
+        background: "rgb(229,231,245)",
         border: "5px solid white",
         borderRadius: "8px",
       }}
