24932caa9c7dc66eebf7eaba3c24179d80329e17
Crystal <crwainstock@gmail.com>
2023-08-21 08:57:08 +0200

    added D3 scripts to head

--- src/Components/Summary.jsx
+++ src/Components/Summary.jsx
@@ -1,6 +1,15 @@
-import { useState } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
 
 function Summary() {
+  const [data, setData] = useState([]);
+  //   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchData();
+    console.log(data);
+  }, []);
+
   return <h2>This is where the summary will display.</h2>;
 }
 
