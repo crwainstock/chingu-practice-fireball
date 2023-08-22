c13beabcb0fdf84dc91b16c189b7b570ade8da29
Crystal <crwainstock@gmail.com>
2023-08-22 13:01:10 +0200

    update by composition styling

--- src/Components/StrikesByComposition.jsx
+++ src/Components/StrikesByComposition.jsx
@@ -64,33 +64,43 @@ function StrikesByComposition() {
       {
         label: "Number of Strikes by Meteorite Composition",
         data: topTenCompositions,
-        borderColor: "rgb(75, 192, 192)",
         fill: false,
+        backgroundColor: "rgb(12, 22, 79)",
+        borderColor: "rgb(12, 22, 79)",
+        pointBackgroundColor: "rgb(12, 22, 79)",
+        pointBorderColor: "rgb(38,182,183)",
+        pointHoverBackgroundColor: "rgb(38,182,183)",
+        pointHoverBorderColor: "rgb(38,182,183)",
       },
     ],
   };
 
   const chartOptions = {
+    layout: {
+      padding: 20,
+    },
     scales: {
       x: {
         ticks: {
-          color: "white",
+          color: "rgb(38,182,183)",
         },
       },
       y: {
         ticks: {
-          color: "white",
+          color: "rgb(38,182,183)",
         },
         beginAtZero: true,
       },
     },
     plugins: {
       legend: {
-        labels: {
-          color: "wheat",
-          font: {
-            size: 16,
-          },
+        display: false,
+      },
+      title: {
+        display: true,
+        text: "Meteorite Strikes by Composition: Top 10",
+        font: {
+          size: 18,
         },
       },
     },
@@ -102,9 +112,10 @@ function StrikesByComposition() {
         width: 700,
         color: "white",
         marginTop: 50,
-        background: "rgb(12, 22, 79)",
-        border: "5px solid white",
-        borderRadius: "3px",
+        background: "rgb(229,231,245)",
+        margin: "1rem",
+        // border: "5px solid white",
+        borderRadius: "8px",
       }}
     >
       <Line data={chartData} options={chartOptions} />
